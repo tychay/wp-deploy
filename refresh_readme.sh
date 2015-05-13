3203e839f011ae0471382ae8340407ec45e05934
terry chay <tychay@php.net>
2015-05-13 15:02:51 -0700

    Refresh readme script
    Can be run before or after
    
    Signed-off-by: terry chay <tychay@php.net>

---
+++ refresh_readme.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+BASEDIR=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
+SRC_DIR=${BASEDIR}/git
+
+if [ ! -d "$SRC_DIR/.git" ]; then
+	echo "$SRC_DIR doesn't seem to be a git repository"
+	exit
+fi
+
+pushd SRC_DIR
+	git pull origin master
+	wp2md convert <readme.txt >README.md
+	git add README.md 
+	git commit -m "refresh README markdown"
+	git push origin master
+popd
